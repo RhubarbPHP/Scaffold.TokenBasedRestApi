56606657069d9369ae9f79af09ae979feff1a646
Michael Miscampbell <mmiscampbell@gcdtech.com>
2019-01-30 11:34:41 +0000

    Adding logic to support revoking an active API Token

--- src/Adapters/RevokeTokenResourceAdapter.php
+++ src/Adapters/RevokeTokenResourceAdapter.php
@@ -7,47 +7,76 @@ use Rhubarb\Crown\LoginProviders\LoginProvider;
 use Rhubarb\Crown\Request\WebRequest;
 use Rhubarb\Crown\Response\NotAuthorisedResponse;
 use Rhubarb\RestApi\Adapters\ModelResourceAdapter;
+use Rhubarb\RestApi\Adapters\ResourceAdapter;
 use Rhubarb\Scaffolds\TokenBasedRestApi\Model\ApiToken;
 use Rhubarb\Scaffolds\TokenBasedRestApi\Resources\TokenResource;
+use Rhubarb\Stem\Filters\Equals;
+use Rhubarb\Crown\DateTime\RhubarbDateTime;
 
-class TokenResourceAdapter extends ModelResourceAdapter
+class RevokeTokenResourceAdapter extends ResourceAdapter
 {
-    /**
-     * @var string
-     */
-    private $loginProviderClassName;
-
-    public function __construct(string $loginProviderClassName = "")
-    {
-        parent::__construct(TokenResource::class, ApiToken::class);
-        $this->loginProviderClassName = $loginProviderClassName;
-    }
-
-    protected function getLoginProvider()
-    {
-        if ($this->loginProviderClassName){
-            $provider = $this->loginProviderClassName;
-            return new $provider();
-        } else {
-            return LoginProvider::getProvider();
-        }
-    }
-
     public function post($payload, $params, WebRequest $request)
     {
-        $loginProvider = $this->getLoginProvider();
+        $token = $this->extractAuthorizationToken($request);
 
-        if (!$loginProvider->isLoggedIn()) {
-            throw new ForceResponseException(new NotAuthorisedResponse($this));
+        try {
+            $apiToken = ApiToken::findFirst(new Equals('Token', $token));
+            $apiToken->Expires = new RhubarbDateTime('-10 seconds');
+            $apiToken->save();
+        } catch (RecordNotFoundException $ex) {
         }
 
-        $model = $loginProvider->getModel();
+        $response = new \stdClass();
+        $response->status = true;
 
-        $token = ApiToken::createToken($model, (isset($_SERVER["REMOTE_ADDR"])) ? $_SERVER["REMOTE_ADDR"] : "cli");
-        $resource = $this->makeResourceFromData($token);
+        return $response;
+    }
 
-        unset($resource->id);
+    private function extractAuthorizationToken(WebRequest $request)
+    {
+        $token = '';
+        if (!$request->header("Authorization")) {
+            Log::debug("Authorization header missing. If using fcgi be sure to instruct Apache to include this header", "RESTAPI");
+            return $token;
+        }
 
-        return $resource;
+        $authString = trim($request->header("Authorization"));
+
+        if (stripos($authString, "token") !== 0 && stripos($authString, "bearer") !== 0) {
+            return $token;
+        }
+
+        if (!preg_match("/(token|bearer)(=|\s+)\"?([[:alnum:]]+)\"?/i", $authString, $match)) {
+            return $token;
+        }
+
+        $token = $match[3];
+
+        return $token;
+    }
+
+    protected function countItems($rangeStart, $rangeEnd, $params, ?WebRequest $request)
+    {
+
+    }
+
+    protected function getItems($rangeStart, $rangeEnd, $params, ?WebRequest $request)
+    {
+
+    }
+
+    public function putResource($resource)
+    {
+
+    }
+
+    public function makeResourceByIdentifier($id)
+    {
+
+    }
+
+    public function makeResourceFromData($data)
+    {
+
     }
 }
